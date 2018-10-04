23567c03920a16da6892c101edc1735635f34ae1
Daydoosh <fumey.david@hotmail.com>
2018-10-04 09:55:56 +0200

    Avancées question 3

--- tp1/hmin317-tp1-master/geometryengine.cpp
+++ tp1/hmin317-tp1-master/geometryengine.cpp
@@ -83,44 +83,49 @@ GeometryEngine::~GeometryEngine()
 
 void GeometryEngine::initPlaneGeometry()
 {
-    VertexData vertices[] = {
-        // face 0
-        {QVector3D(-1.0f, -1.0f,  0.0f), QVector2D(0.0f, 0.0f)},  // v0
-        {QVector3D( 1.0f, -1.0f,  0.0f), QVector2D(0.33f, 0.0f)}, // v1
-        {QVector3D(-1.0f,  1.0f,  0.0f), QVector2D(0.0f, 0.5f)},  // v2
-        {QVector3D( 1.0f,  1.0f,  0.0f), QVector2D(0.33f, 0.5f)}, // v3
+    VertexData vertices[256];
+    for( int i=0 ; i<16 ; i++ )
+    {
+        for( int j=0 ; j<16 ; j++ )
+        {
+            vertices[i*16+j] = {QVector3D( i,  j,  0.0f), QVector2D(0.33f, 0.5f)};
+        }
+    }
 
-        // Vertex data for face 1
-        {QVector3D( 1.0f, -1.0f,  0.0f), QVector2D( 0.0f, 0.5f)}, // v4
-        {QVector3D( 3.0f, -1.0f,  0.0f), QVector2D(0.33f, 0.5f)}, // v5
-        {QVector3D( 1.0f, 1.0f,  0.0f), QVector2D(0.0f, 1.0f)},  // v6
-        {QVector3D( 3.0f, 1.0f,  0.0f), QVector2D(0.33f, 1.0f)}, // v7
+    std::vector<int> indices;
+    GLushort indice[512];
 
-        // Vertex data for face 2
-        {QVector3D( 3.0f, -1.0f,  0.0f), QVector2D(0.66f, 0.5f)}, // v8
-        {QVector3D( 5.0f, -1.0f,  0.0f), QVector2D(1.0f, 0.5f)},  // v9
-        {QVector3D( 3.0f,  1.0f,  0.0f), QVector2D(0.66f, 1.0f)}, // v10
-        {QVector3D( 5.0f,  1.0f,  0.0f), QVector2D(1.0f, 1.0f)},  // v11
+    int k = 0;
+    for( int i=0 ; i<15 ; i++ )
+    {
+        for( int j=0 ; j<15 ; j++ )
+        {
+            if (j%16 != 0)
+            {   indice[k] = i*16+j;
+                k++;} // dédoublement du premier pour liaison
 
-        // Vertex data for face 3
-        {QVector3D(-3.0f, -1.0f,  0.0f), QVector2D(0.66f, 0.0f)}, // v12
-        {QVector3D(-1.0f, -1.0f,  0.0f), QVector2D(1.0f, 0.0f)},  // v13
-        {QVector3D(-3.0f,  1.0f,  0.0f), QVector2D(0.66f, 0.5f)}, // v14
-        {QVector3D(-1.0f,  1.0f,  0.0f), QVector2D(1.0f, 0.5f)},  // v15
+    // Triangle de la face
+            /*
+            indices.push_back(i*16 + j);
+            indices.push_back(i*16 + j+1);
+            indices.push_back(i*16 + j+16);
+            indices.push_back(i*16 + j+1+16);
+            */
+            indice[k] = i*16+j;
+            k++;
+            indice[k] = i*16+j+1;
+            k++;
+            indice[k] = i*16+j+16;
+            k++;
+            indice[k] = i*16+j+1+16;
+            k++;
+    if (j%15 != 0)
+    {  indice[k] = i*16+j+1+16;
+        k++; } // dédoublement du dernier pour liaison
+        }
+    }
 
-        // Vertex data for face 4
-        {QVector3D(-1.0f, -3.0f,  0.0f), QVector2D(0.33f, 0.0f)}, // v16
-        {QVector3D( 1.0f, -3.0f,  0.0f), QVector2D(0.66f, 0.0f)}, // v17
-        {QVector3D(-1.0f, -1.0f,  0.0f), QVector2D(0.33f, 0.5f)}, // v18
-        {QVector3D( 1.0f, -1.0f,  0.0f), QVector2D(0.66f, 0.5f)}, // v19
 
-        // Vertex data for face 5
-        {QVector3D(-1.0f,  1.0f,  0.0f), QVector2D(0.33f, 0.5f)}, // v20
-        {QVector3D( 1.0f,  1.0f,  0.0f), QVector2D(0.66f, 0.5f)}, // v21
-        {QVector3D(-1.0f,  3.0f,  0.0f), QVector2D(0.33f, 1.0f)}, // v22
-        {QVector3D( 1.0f,  3.0f,  0.0f), QVector2D(0.66f, 1.0f)}  // v23
-
-    };
     // Indices for drawing cube faces using triangle strips.
     // Triangle strips can be connected by duplicating indices
     // between the strips. If connecting strips have opposite
@@ -128,24 +133,18 @@ void GeometryEngine::initPlaneGeometry()
     // index of the second strip needs to be duplicated. If
     // connecting strips have same vertex order then only last
     // index of the first strip needs to be duplicated.
-    GLushort indices[] = {
-         0,  1,  2,  3,  3,     // Face 0 - triangle strip ( v0,  v1,  v2,  v3)
-         4,  4,  5,  6,  7,  7, // Face 1 - triangle strip ( v4,  v5,  v6,  v7)
-         8,  8,  9, 10, 11, 11, // Face 2 - triangle strip ( v8,  v9, v10, v11)
-        12, 12, 13, 14, 15, 15, // Face 3 - triangle strip (v12, v13, v14, v15)
-        16, 16, 17, 18, 19, 19, // Face 4 - triangle strip (v16, v17, v18, v19)
-        20, 20, 21, 22, 23      // Face 5 - triangle strip (v20, v21, v22, v23)
-    };
+
+
 
 //! [1]
     // Transfer vertex data to VBO 0
     // Initialise le buffer de la taille souhaitée, et le remplit avec les données voulues
     arrayBuf.bind();
-    arrayBuf.allocate(vertices, 24 * sizeof(VertexData));
+    arrayBuf.allocate(vertices, 256 * sizeof(VertexData));
 
     // Transfer index data to VBO 1
     indexBuf.bind();
-    indexBuf.allocate(indices, 34 * sizeof(GLushort));
+    indexBuf.allocate(indice, 512 * sizeof(GLushort));
 //! [1]
 
 }
@@ -224,6 +223,39 @@ void GeometryEngine::initCubeGeometry()
 }
 */
 //! [2]
+
+void GeometryEngine::drawPlaneGeometry(QOpenGLShaderProgram *program)
+{
+    // Tell OpenGL which VBOs to use
+    // Indique quels buffers seront les buffers courants, donc lesquels utiliser
+    arrayBuf.bind();
+    indexBuf.bind();
+
+    // Offset for position
+    quintptr offset = 0;
+
+    // Tell OpenGL programmable pipeline how to locate vertex position data
+    // VertexLocation représente l'ID du buffer que l'on veut utiliser
+    int vertexLocation = program->attributeLocation("a_position");
+    program->enableAttributeArray(vertexLocation);
+    program->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(VertexData));
+
+    // Offset for texture coordinate
+    // Offset indique quoi afficher. Si = 0, on affiche tout ce qu'il y a dans le buffer.
+    // Ici, l'offset vaut la taille dela première valeur de notre tableau (position géométrique des points)
+    // Car on désire utiliser uniquement la seconde valeur (QVector3D, et QVector2D),
+    offset += sizeof(QVector3D);
+
+    // Tell OpenGL programmable pipeline how to locate vertex texture coordinate data
+    int texcoordLocation = program->attributeLocation("a_texcoord");
+    program->enableAttributeArray(texcoordLocation);
+    program->setAttributeBuffer(texcoordLocation, GL_FLOAT, offset, 2, sizeof(VertexData));
+
+    // Draw cube geometry using indices from VBO 1
+    glDrawElements(GL_TRIANGLE_STRIP, 512, GL_UNSIGNED_SHORT, 0);
+}
+
+/*
 void GeometryEngine::drawCubeGeometry(QOpenGLShaderProgram *program)
 {
     // Tell OpenGL which VBOs to use
@@ -254,4 +286,5 @@ void GeometryEngine::drawCubeGeometry(QOpenGLShaderProgram *program)
     // Draw cube geometry using indices from VBO 1
     glDrawElements(GL_TRIANGLE_STRIP, 34, GL_UNSIGNED_SHORT, 0);
 }
+*/
 //! [2]
--- tp1/hmin317-tp1-master/mainwidget.cpp
+++ tp1/hmin317-tp1-master/mainwidget.cpp
@@ -187,7 +187,7 @@ void MainWidget::resizeGL(int w, int h)
     qreal aspect = qreal(w) / qreal(h ? h : 1);
 
     // Set near plane to 3.0, far plane to 7.0, field of view 45 degrees
-    const qreal zNear = 3.0, zFar = 7.0, fov = 90.0;
+    const qreal zNear = 2.0, zFar = 20.0, fov = 140.0;
 
     // Reset projection
     projection.setToIdentity();
@@ -218,5 +218,6 @@ void MainWidget::paintGL()
     program.setUniformValue("texture", 0);
 
     // Draw cube geometry
-    geometries->drawCubeGeometry(&program);
+    //geometries->drawCubeGeometry(&program);
+    geometries->drawPlaneGeometry(&program);
 }
